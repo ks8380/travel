f590484454db074e48ca99a1f8fb773f0983e9ac
ks8380 <838065298@qq.com>
2019-02-19 18:36:10 +0800

    commit

--- src/main.js
+++ src/main.js
@@ -4,8 +4,8 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import fastClick from 'fastclick'
-import './assets/styles/reset.css'
-import './assets/styles/border.css'
+// import './assets/styles/reset.css'
+// import './assets/styles/border.css'
 
 Vue.config.productionTip = false
 
--- src/pages/Home/Home.vue
+++ src/pages/Home/Home.vue
@@ -1,5 +1,8 @@
 <template>
-  <div>home</div>
+  <div>
+    <div>home</div>
+    <router-link to="list">list</router-link>
+  </div>
 </template>
 
 <script>
@@ -9,4 +12,4 @@ export default {
 </script>
 
 <style >
-</style>
+</style> 
